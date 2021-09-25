5851ccd89b398fdd50be3bbd5ab5cbb598c1c638
Daniel Figueiredo <danielfigueiredo18@outlook.com>
2021-09-25 15:33:49 -0300

    Request da lista de pokemons

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
 <template>
   <div id="app">
-
+    <div v-for="(poke,index) in pokemons" :key="index">
+      <Pokemom :name="poke.name" :url="poke.url" :num="index+1" />
+    </div>
   </div>
 </template>
 
 <script>
-
+import axios from 'axios'
+import Pokemom from './components/Pokemon'
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return {
+      pokemons: []
+      }
+  },
+  created: function(){
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0').then(res =>{
+      this.pokemons = res.data.results
+      console.log(this.pokemons)
+    })
+  },
+  components:{
+    Pokemom
+  }
 }
 </script>
 
